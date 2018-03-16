c3ea2cffb6081f47b5b79735060b4f68bf43d43e
Joey <joeybailey284@gmail.com>
2018-03-16 11:54:58 -0400

    added percent values

--- countLetters.py
+++ countLetters.py
@@ -1,18 +1,24 @@
-hello = "hello, and good day to all of the cats in the cupboard"
+hello = "ABBDCDBCBACDBCDDCBBADCBABCDCDCABABCDBCADACDCBACDBDCABDDCABDCDDDBACDBACDCCB"
 tally_a = 0
 tally_b = 0
 tally_c = 0
 tally_d = 0
 count = 0
 for letter in hello:
-    if letter == 'c':
+    if letter == 'C':
         tally_c +=1
-    if letter == 'b':
+    if letter == 'B':
         tally_b +=1
-    if letter == 'a':
+    if letter == 'A':
         tally_a +=1
-    if letter == 'd ':
+    if letter == 'D':
         tally_d +=1
-    count +=1
+    count += 1
 print("total: " + str(count)) 
-print("C: " + str(tally_c) + " G: " + str(tally_b) + " A: " + str(tally_a) + " T: " + str(tally_d))
+print("A: " + str(tally_a) + " B: " + str(tally_b) + " C: " + str(tally_c) + " D: " + str(tally_d))
+A_percent = tally_a*100//count
+B_percent = tally_b*100//count
+C_percent = tally_c*100//count
+D_percent = tally_d*100//count
+print(str(A_percent) + "% is a, " + str(B_percent) + "% is b, " +
+      str(C_percent) + "% is c, " + str(D_percent) + "% is d")
