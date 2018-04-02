4dea65eab9861d8f3681837e1cdfb708de7977d1
Joey <joeybailey284@gmail.com>
2018-04-02 11:06:24 -0400

    add cheat sheet

---
+++ gitCheatSheet.py
@@ -0,0 +1,10 @@
+#git cheat sheet
+
+#cd = change directory.
+#cd /c puts you in the c drive.
+
+#git add adds a file to the branch
+
+#git commit -m "" commits the added files.
+#git push origin master pushes the commited files from the current branch
+#(master) to the new one (origin)
